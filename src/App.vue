354707fc38713ebf0c6bbbeb824f8034496cd7b7
amrowael <aboelsoof12@gmail.com>
2022-08-08 19:18:19 +0200

    released v-1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container bg-dark">
+    <Quote :writer="writers[localI]" :quoteTxt="texts[localI]" />
+
+    <div class="btns mt-5 d-flex gap-2">
+      <button class="btn btn-success" @click="localI = 0">to start</button>
+      <button class="btn btn-success" :disabled="localI === writers.length-1" @click="localI++">next</button>
+      <button class="btn btn-success" :disabled="localI == 0" @click="localI--">previuos</button>
+      <button class="btn btn-success" @click="toEnd(writers)">to end</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quote from './components/Quote.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Quote
+  },
+  data() {
+    return {
+      texts: [],
+      writers: [],
+      apiUrl: 'https://type.fit/api/quotes',
+      index: 0,
+      localI: localStorage.getItem('index')
+    }
+  },
+  methods: {
+    randomize(arr) {
+      return Math.round( Math.random() * arr.length )
+    },
+    toEnd(arr) {
+      this.localI = (arr.length -1)
+      console.log(arr.length -1);
+    }
+  },
+  mounted() {
+    
+    fetch(this.apiUrl)
+    .then(response => response.json() )
+    .then(response => {
+      for (let i = 0; i < response.length; i++) {
+        this.texts.push(response[i].text)
+        this.writers.push(response[i].author)
+      }
+    })
+
+    setInterval( () => {
+      window.localStorage.setItem('index', this.localI);
+    }, 100)
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
 }
+
+body {
+  background-color: rgb(46, 46, 46);
+  overflow-x: hidden;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+}
+
+.container {
+  background-color: rgb(46, 46, 46);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="quote">
+    <!-- {{ handleApi() }} -->
+    <h1>{{ quoteTxt }}</h1> 
+    <p>{{ writer }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'myQ',
+    props: ['quoteTxt', 'writer']
+}
+</script>
+
+<style lang="scss">
+    .quote {
+        position: relative;
+        // left: 25px;
+        padding: 40px 20px;
+        font-family: 'Montserrat', sans-serif;
+        // margin: 25px 10px;
+        color: white;
+        border: 6px solid #198754;
+        border-top: none;
+        border-right: none;
+        width: 80%;
+        // box-shadow: -10px 10px 20px rgb(89, 255, 0);
+        background-color: rgb(72, 72, 72);
+        border-radius: 10px;
+
+        h1 {
+            margin-bottom: 25px;
+        }
+    }
+</style>
